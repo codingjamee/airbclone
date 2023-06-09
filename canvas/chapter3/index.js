81e3df8ff179ac8ecf00ee561dca0c874cd25b72
codingjamee <codingjamee@gmail.com>
2023-06-09 13:26:57 +0900

    resize와 load될 때마다 초기화 해주기, 로드될 때마다 렌더해주기

--- canvas/chapter3/index.js
+++ canvas/chapter3/index.js
@@ -2,30 +2,44 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const dpr = window.devicePixelRatio;
 
-const canvasWidth = innerWidth;
-const canvasHeight = innerHeight;
+let canvasWidth = innerWidth;
+let canvasHeight = innerHeight;
 const fps = 60;
 const interval = 1000 / fps;
 
-canvas.width = canvasWidth * dpr;
-canvas.height = canvasHeight * dpr;
+function init() {
+  canvasWidth = innerWidth;
+  canvasHeight = innerHeight;
+  canvas.width = canvasWidth * dpr;
+  canvas.height = canvasHeight * dpr;
 
-canvas.style.width = canvasWidth + "px";
-canvas.style.height = canvasHeight + "px";
+  canvas.style.width = canvasWidth + "px";
+  canvas.style.height = canvasHeight + "px";
 
-ctx.scale(dpr, dpr);
+  ctx.scale(dpr, dpr);
+}
 
-let then = Date.now();
-let now, delta;
+function render() {
+  let then = Date.now();
+  let now, delta;
 
-const frame = () => {
+  const frame = () => {
+    requestAnimationFrame(frame);
+    delta = now - then;
+    if (delta < interval) return;
+
+    console.log("frame");
+
+    then = now - (delta % interval);
+  };
   requestAnimationFrame(frame);
-  delta = now - then;
-  if (delta < interval) return;
+}
 
-  console.log("frame");
+window.addEventListener("load", () => {
+  init();
+  render();
+});
 
-  then = now - (delta % interval);
-};
-
-requestAnimationFrame(frame);
+window.addEventListener("resize", () => {
+  init();
+});
