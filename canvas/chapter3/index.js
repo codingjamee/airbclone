46790b8e6ec264039e58e2b98f75ff1bb6a2f7d1
codingjamee <codingjamee@gmail.com>
2023-06-09 13:57:10 +0900

    파티클 중앙에서 원모양으로 떨어진 particle들 찍어보기

--- canvas/chapter3/index.js
+++ canvas/chapter3/index.js
@@ -1,3 +1,5 @@
+import Particle from "./js/Particle.js";
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const dpr = window.devicePixelRatio;
@@ -6,6 +8,8 @@ let canvasWidth = innerWidth;
 let canvasHeight = innerHeight;
 const fps = 60;
 const interval = 1000 / fps;
+
+const particles = [];
 
 function init() {
   canvasWidth = innerWidth;
@@ -19,6 +23,13 @@ function init() {
   ctx.scale(dpr, dpr);
 }
 
+function createRing() {
+  const PARTICLE_NUM = 20;
+  for (let i = 0; i < PARTICLE_NUM; i++) {
+    particles.push(new Particle());
+  }
+}
+
 function render() {
   let then = Date.now();
   let now, delta;
@@ -28,7 +39,10 @@ function render() {
     delta = now - then;
     if (delta < interval) return;
 
-    console.log("frame");
+    particles.forEach((particle, index) => {
+      particle.update();
+      particle.draw(ctx);
+    });
 
     then = now - (delta % interval);
   };
@@ -42,4 +56,8 @@ window.addEventListener("load", () => {
 
 window.addEventListener("resize", () => {
   init();
+});
+
+window.addEventListener("click", () => {
+  createRing();
 });
